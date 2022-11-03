73a1fd5f741567ea785a29466e3e52ce64a5cedd
Daniel Clark <dan_clark@outlook.com>
2022-11-03 13:42:36 +0000

    Progress ring work sort of completed

--- UI/Avalonia.Controls/ProgressRing/ProgressRing.cs
+++ UI/Avalonia.Controls/ProgressRing/ProgressRing.cs
@@ -7,7 +7,15 @@ namespace TheXamlGuy.UI.Avalonia.Controls
     public class ProgressRing : RangeBase
     {
         public static readonly StyledProperty<bool> IsIndeterminateProperty =
-            AvaloniaProperty.Register<ProgressRing, bool>(nameof(IsIndeterminate), true);
+            AvaloniaProperty.Register<ProgressRing, bool>(nameof(IsIndeterminate), false);
+
+        public static readonly StyledProperty<ProgressRingTemplateSettings> TemplateSettingsProperty =
+            AvaloniaProperty.Register<ProgressRing, ProgressRingTemplateSettings>(nameof(TemplateSettings));
+
+        public ProgressRing()
+        {
+            SetValue(TemplateSettingsProperty, new ProgressRingTemplateSettings());
+        }
 
         public bool IsIndeterminate
         {
@@ -15,23 +23,40 @@ namespace TheXamlGuy.UI.Avalonia.Controls
             set => SetValue(IsIndeterminateProperty, value);
         }
 
+        public ProgressRingTemplateSettings TemplateSettings
+        {
+            get => GetValue(TemplateSettingsProperty);
+            set => SetValue(TemplateSettingsProperty, value);
+        }
+
         protected override void OnApplyTemplate(TemplateAppliedEventArgs args)
         {
             UpdateIsIndeterminate();
+            UpdateValue();
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+            if (args.Property == IsIndeterminateProperty)
+            {
+                UpdateIsIndeterminate();
+            }
+            
+            if (args.Property == ValueProperty)
+            {
+                UpdateValue();
+            }
         }
 
         private void UpdateIsIndeterminate()
         {
             PseudoClasses.Set(":indeterminate", IsIndeterminate);
-
         }
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+
+        private void UpdateValue()
         {
-            base.OnPropertyChanged(change);
-            if (change.Property == IsIndeterminateProperty)
-            {
-                UpdateIsIndeterminate();
-            }
+            TemplateSettings.SetValue(ProgressRingTemplateSettings.SweepAngleProperty, Value == Maximum ? 360 : 360 * (Value / (Maximum - Minimum)));
         }
     }
 }
---
+++ UI/Avalonia.Controls/ProgressRing/ProgressRingTemplateSettings.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace TheXamlGuy.UI.Avalonia.Controls
+{
+    public class ProgressRingTemplateSettings : TemplatedControl
+    {
+        public static readonly StyledProperty<double> SweepAngleProperty =
+            AvaloniaProperty.Register<ProgressRingTemplateSettings, double>(nameof(SweepAngle), 0);
+
+        public double SweepAngle
+        {
+            get => GetValue(SweepAngleProperty);
+            set => SetValue(SweepAngleProperty, value);
+        }
+    }
+}
